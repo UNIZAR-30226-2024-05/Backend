77fbb481b551d5d8bb57f298e01b8accef703bf2
840091 <840091@unizar.es>
2024-04-03 17:30:29 +0200

    Añadido audiolibro

--- sql/02_data.sql
+++ sql/02_data.sql
@@ -1,12 +1,5 @@
-INSERT INTO users (username, mail, password, admin) VALUES
-('defaultAdmin', 'defaultAdmin@gmail.com', 'passwdAdmin', true);
-
 INSERT INTO autores (nombre) VALUES
-('Lope de Vega'),
-('Julio Verne'),
-('Miguel de Unamuno'),
-('Pierre Reverdy'),
-('Oscar Wilde');
+('J. K. Rowling');
 
 INSERT INTO generos (nombre) VALUES
 ('Romance'),
@@ -18,77 +11,28 @@ INSERT INTO generos (nombre) VALUES
 ('Suspense');
 
 INSERT INTO audiolibros (titulo, autor, descripcion, img) VALUES
-('Audiolibro 1', 1, 'Descripción del audiolibro 1', 'imagen1.jpg'),
-('Audiolibro 2', 2, 'Descripción del audiolibro 1', 'imagen1.jpg'),
-('Audiolibro 3', 1, 'Descripción del audiolibro 1', 'imagen1.jpg'),
-('Audiolibro 4', 1, 'Descripción del audiolibro 1', 'imagen1.jpg'),
-('Audiolibro 5', 2, 'Descripción del audiolibro 2', 'imagen2.jpg');
+('Harry Potter y la piedra filosofal', 1, '«Con las manos temblorosas, Harry le dio la vuelta al sobre y vio un sello de lacre púrpura con un escudo de armas: un león, un águila, un tejón y una serpiente, que rodeaban una gran letra H.»
+Harry Potter nunca ha oído hablar de Hogwarts hasta que empiezan a caer cartas en el felpudo del número 4 de Privet Drive. Llevan la dirección escrita con tinta verde en un sobre de pergamino amarillento con un sello de lacre púrpura, y sus horripilantes tíos se apresuran a confiscarlas. Más tarde, el día que Harry cumple once años, Rubeus Hagrid, un hombre gigantesco cuyos ojos brillan como escarabajos negros, irrumpe con una noticia extraordinaria: Harry Potter es un mago, y le han concedido una plaza en el Colegio Hogwarts de Magia y Hechicería. ¡Está a punto de comenzar una aventura increíble!', 
+'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1.jpeg');
 
 INSERT INTO genero_audiolibro (audiolibro, genero) VALUES
-(1, 1),
-(1, 2),
-(2, 2);
+(1, 3);
 
 INSERT INTO capitulos (numero, nombre, audiolibro, audio) VALUES
-(1, 'Capítulo 1', 1, 'audio1.mp3'),
-(2, 'Capítulo 2', 1, 'audio2.mp3'),
-(1, 'Capítulo 1', 2, 'audio3.mp3');
-
-/*
-INSERT INTO amigos (user1, user2) VALUES
-(1, 2),
-(2, 3),
-(4, 1);
-
-INSERT INTO peticiones (sender, receiver, estado, fecha) VALUES
-(1, 2, '2', CURRENT_TIMESTAMP),
-(2, 3, '2', CURRENT_TIMESTAMP),
-(4, 1, '2', CURRENT_TIMESTAMP),
-(1, 3, '1', CURRENT_TIMESTAMP);
-
-INSERT INTO reviews (usuario, audiolibro, comentario, puntuacion, visibilidad, fecha) VALUES
-(1, 1, 'Buena historia, me encantó', 5, '1', CURRENT_TIMESTAMP),
-(2, 2, 'Interesante pero podría mejorar', 3, '1', CURRENT_TIMESTAMP);
-
-
-INSERT INTO club_lectura (nombre, audiolibro, descripcion) VALUES
-('Club de Lectura 1', 1, 'Club para discutir el audiolibro 1'),
-('Club de Lectura 2', 2, 'Club para discutir varios audiolibros');
-
-INSERT INTO marcapaginas (titulo, capitulo, usuario, tipo, fecha) VALUES
-('Marcador Capítulo 1', 1, 1, '0', '12:00:00'),
-('Marcador Capítulo 2', 2, 1, '0', '12:00:00'),
-('Marcador Capítulo 1', 1, 2, '1', '13:30:00');
-
-INSERT INTO miembros_club (club, usuario) VALUES
-(1, 1),
-(1, 2),
-(1, 4),
-(2, 3),
-(2, 1);
-
-INSERT INTO mensajes (club, usuario, mensaje, fecha) VALUES
-( 1, 1, 'usuario 1 manda mensaje a club 1', CURRENT_TIMESTAMP),
-( 1, 4, 'usuario 4 manda mensaje a club 1', CURRENT_TIMESTAMP),
-( 2, 1, 'usuario 1 manda mensaje a club 2', CURRENT_TIMESTAMP),
-( 2, 3, 'usuario 3 manda mensaje a club 2', CURRENT_TIMESTAMP);
-
-INSERT INTO colecciones (titulo, propietario) VALUES
-('coleccion1', 1),
-('coleccion2', 1),
-('coleccion1', 4);
-
-INSERT INTO colecciones_usuarios (coleccion, usuario) VALUES
-(1, 1),
-(1, 2),
-(1, 4),
-(2, 3),
-(3, 1);
-
-INSERT INTO colecciones_audiolibros (coleccion, audiolibro) VALUES
-(1, 1),
-(1, 2),
-(1, 4),
-(2, 3),
-(2, 5);
-*/
+(1, 'El niño que sobrevivió', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_1.mp3'),
+(2, 'El vidrio que se desvaneció', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_2.mp3'),
+(3, 'Las cartas de nadie', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_3.mp3'),
+(4, 'El guardián de las llaves', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_4.mp3'),
+(5, 'El callejón Diagon', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_5.mp3'),
+(6, 'El viaje a través del andén nueve y tres cuartos', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_6.mp3'),
+(7, 'El Sombrero Seleccionador', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_7.mp3'),
+(8, 'El profesor de Pociones', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_8.mp3'),
+(9, 'El duelo a medianoche', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_9.mp3'),
+(10, 'Halloween', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_10.mp3'),
+(11, 'Quidditch', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_11.mp3'),
+(12, 'El espejo de Oesed', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_12.mp3'),
+(13, 'Nicolás Flamel', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_13.mp3'),
+(14, 'Norberto, el ridgeback noruego', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_14.mp3'),
+(15, 'El bosque prohibido', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_15.mp3'),
+(16, 'A través de la trampilla', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_16.mp3'),
+(17, 'El hombre con dos caras', 1, 'https://narrativesdb.blob.core.windows.net/archivos-narratives/HarryPotter1_17.mp3');
