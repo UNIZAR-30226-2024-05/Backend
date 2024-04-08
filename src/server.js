bc0a2d53dfbf69f0d79ac647247775c5519cff35
Daniel <158586841+daniel-844388@users.noreply.github.com>
2024-04-09 00:02:04 +0200

    Rutas amistad en server.js

--- src/server.js
+++ src/server.js
@@ -34,12 +34,14 @@ module.exports = io;
 const userRoutes = require("./routes/userRoutes");
 const audiolibrosRoutes = require("./routes/audiolibrosRoutes");
 const coleccionesRoutes = require("./routes/coleccionesRoutes");
+const amistadRoutes = require("./routes/amistadRoutes");
 
 const PORT = process.env.PORT || 8000;
 
 app.use("/users", userRoutes);
 app.use("/audiolibros", audiolibrosRoutes);
 app.use("/colecciones", coleccionesRoutes);
+app.use("/amistad", amistadRoutes);
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
