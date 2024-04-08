8b8b287723a564b7845210648672c19a9a075ec7
Lilai <129078707+840091@users.noreply.github.com>
2024-04-08 18:14:12 +0200

    Delete src directory

--- src/routes/audiolibrosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const audiolibrosController = require("../controllers/audiolibrosController");
-
-router.get('/genre/:genero', audiolibrosController.getAudiolibrosByGenero);
-
-module.exports = router;
