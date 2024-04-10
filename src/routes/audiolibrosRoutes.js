8654c47d6b136221b3c585fb47b72a4c21715d14
840091 <840091@unizar.es>
2024-04-10 17:41:51 +0200

    Corrección rutas audiolibros

--- src/routes/audiolibrosRoutes.js
+++ src/routes/audiolibrosRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const audiolibrosController = require("../controllers/audiolibrosController");
-const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/', authMiddleware.isAuthenticated, audiolibrosController.getAllAudiolibros);
-router.get('/:genero', authMiddleware.isAuthenticated, audiolibrosController.getAudiolibrosByGenero);
+router.get('/', audiolibrosController.getAllAudiolibros);
+router.get('/:genero', audiolibrosController.getAudiolibrosByGenero);
 
 module.exports = router;
