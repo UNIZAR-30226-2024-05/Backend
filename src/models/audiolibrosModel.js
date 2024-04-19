180b60327d937c743596c2416b33b3f88a277d13
840091 <840091@unizar.es>
2024-04-19 15:48:18 +0200

    Puntuación devuelve 0.00 en vez de null

--- src/models/audiolibrosModel.js
+++ src/models/audiolibrosModel.js
@@ -4,7 +4,7 @@ const AudiolibrosModel = {
     async getAllAudiolibros() {
         try {
             const audiolibros = await pool.query(`
-                SELECT a.*, g.nombre AS genero, ROUND(AVG(r.puntuacion)::numeric, 2) AS puntuacion
+                SELECT a.*, g.nombre AS genero, COALESCE(ROUND(AVG(r.puntuacion)::numeric, 2), 0.00) AS puntuacion
                 FROM audiolibros a
                 JOIN genero_audiolibro ga ON a.id = ga.audiolibro
                 JOIN generos g ON ga.genero = g.id
