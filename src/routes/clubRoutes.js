30e63819851142d7b2a06a05fc46731757bc358a
Daniel <844388@unizar.es>
2024-05-02 21:07:57 +0200

    Corrección conflicto lista

--- src/routes/clubRoutes.js
+++ src/routes/clubRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const clubController = require("../controllers/clubController");
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/:id', authMiddleware.isAuthenticated,clubController.DatosDelClub);
+router.get('/datos/:id', authMiddleware.isAuthenticated,clubController.DatosDelClub);
 router.get('/lista', authMiddleware.isAuthenticated, clubController.getClubesOfUser);
 /*router.get('/all', clubController.AllClubs);  Habra que hablar si sera implementar buscador, si les devolvemos todos y ellos gestionan el buscador o si los devolvemos por audiolibro*/
 router.post('/create', authMiddleware.isAuthenticated, clubController.CrearClub);
