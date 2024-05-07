18eaff3c206c365b893fad2b2082a5b48e4d0cbb
840091 <840091@unizar.es>
2024-05-07 15:25:41 +0200

    Añadido id de autor en consulta de todos los audiolibros

--- src/models/audiolibrosModel.js
+++ src/models/audiolibrosModel.js
@@ -38,7 +38,7 @@ const AudiolibrosModel = {
     async getAllAudiolibros() {
         try {
             const audiolibros = await pool.query(`
-                SELECT a.id, a.titulo, aut.nombre AS autor, g.nombre AS genero, a.descripcion, a.img, 
+                SELECT a.id, a.titulo, a.autor AS autor_id, aut.nombre AS autor, g.nombre AS genero, a.descripcion, a.img, 
                 COALESCE(ROUND(AVG(r.puntuacion)::numeric, 2), 0.00) AS puntuacion
                 FROM audiolibros a
                 JOIN genero_audiolibro ga ON a.id = ga.audiolibro
